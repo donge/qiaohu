becbb2318ac30d490b2f1bebf94623e02a2480b5
Dongdong Yang <donge@donge.org>
2026-04-09 22:08:35 +0800

    build: add release signing config

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,10 +20,20 @@ android {
         }
     }
 
+    signingConfigs {
+        create("release") {
+            storeFile = file("qiaohu-release.jks")
+            storePassword = "qiaohu123"
+            keyAlias = "qiaohu"
+            keyPassword = "qiaohu123"
+        }
+    }
+
     buildTypes {
         release {
             isMinifyEnabled = false
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+            signingConfig = signingConfigs.getByName("release")
         }
         debug {
             isDebuggable = true
